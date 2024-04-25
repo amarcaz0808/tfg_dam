51c2114d04882ce7567cc338d6965a29f132c4c3
amarcaz0808 <98592893+amarcaz0808@users.noreply.github.com>
2024-04-25 16:18:48 +0200

    iframes añadidos

--- SimplePlatformer_TFG/Assets/Scripts/Health/Health.cs
+++ SimplePlatformer_TFG/Assets/Scripts/Health/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
@@ -11,10 +12,16 @@ public class Health : MonoBehaviour
     private Animator anim;
 
     //Primitive Variables
+    [Header("Health")] //This is merely for organizational purposes, it will separate different fields in "categories" in Unity, making it easier to see where everything is
     [SerializeField] private float startingHealth;
-
     public float currentHealth { get; private set; } //Get it anywhere, set it privately
     private bool dead; //Just to make sure the dying animation won't play twice
+
+    [Header("iFrames")]
+    //Against how I was getting used to organize and comment my code, after a Header the first field has to be either public or a SerializeField. No biggie tho!
+    [SerializeField] private float iFrameDuration; //For how long will the flashes take place
+    [SerializeField] private int flashesAmount; //How many flashes there will be
+    private SpriteRenderer spriteRenderer; //Make the flashes happen
 
     /**
      * METHODS
@@ -24,6 +31,7 @@ public class Health : MonoBehaviour
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }//EndOf method Awake
 
     // Start is called before the first frame update
@@ -60,6 +68,7 @@ public class Health : MonoBehaviour
             //Still alive!
             anim.SetTrigger("hurt");
             //TODO - IFrames
+            StartCoroutine(Invulnerability()); //Start this method in a separate thread
         }
         else
         {
@@ -69,7 +78,7 @@ public class Health : MonoBehaviour
                 anim.SetTrigger("die");
                 GetComponent<PlayerMovement>().enabled = false;
                 dead = true;
-            }
+            }//EndOf IF checking bool dead == false
         }//EndOf IF/ELSE checking whether or not Player is dead
     }//EndOf method TakeDamage
 
@@ -78,4 +87,21 @@ public class Health : MonoBehaviour
         //Makes Player regain health, to be later elaborated on
         currentHealth = Mathf.Clamp(currentHealth + _healing, 0, startingHealth);
     }//EndOf method RecoverHealth
+
+    private IEnumerator Invulnerability()
+    {
+        //Deactivate collisions between set layers
+        Physics2D.IgnoreLayerCollision(8, 9, true); //Layer8 is Player; Layer9 is Enemy; TRUE, the collisions will be ignored
+
+        for (int i = 0; i < flashesAmount; i++)
+        {
+            spriteRenderer.color = new Color(1f, 0f, 0f, 0.5f); //RED, GREEN, BLUE, ALPHA (Transparency)
+            yield return new WaitForSeconds(iFrameDuration / (flashesAmount * 2)); //Wair for a bit before continuing
+            spriteRenderer.color = Color.white; //Go back to the regular colors (not actually white, it's kinda weird but, play a bit with Unity's Sprite Color parameters and you'll see)
+            yield return new WaitForSeconds(iFrameDuration / (flashesAmount * 2)); //Wait just once more (before the loop laps over, that is)
+        }//EndOf FOR-Loop
+
+        //Reactivate collisions between set layers
+        Physics2D.IgnoreLayerCollision(8, 9, false); //Same as before, just re-activate collisions
+    }//EndOf IEnumerator Invulnerability
 }
